939af8970a6e4d1ef27ddd7e13c7282aacfd1f79
leothorp <leothorp@users.noreply.github.com>
2022-07-04 21:24:52 -0500

    log

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ In your project directory:
 ### Usage Example
 
 ```js
+
 import { createLogger } from "@leothorp/loggle";
 
-
-const log = createLogger({ level: 'debug' });
+const log = createLogger({ level: "debug" });
 log.critical("critical");
 log.error("error");
 log.warn("warning");
@@ -36,39 +36,51 @@ log.debug(
   { prefix: { format: (parts: string[]) => `((${parts.join("__")})):` } },
   "a debug message"
 );
+
 ```
+
 ### Output
 
 <img width="646" alt="image" src="https://user-images.githubusercontent.com/12928449/177232719-5e97e1b8-85cb-4b48-b10a-16080b3a00e1.png">
 
-
 ### Configuration
 
-`createLogger` accepts a single optional configuration object as a parameter. This object can contain various config parameters. Default values and explanations for each property 
+`createLogger` accepts a single optional configuration object as a parameter. This object can contain various config parameters. Default values and explanations for each property
 are below (taken from src/index.js).
 
 It's only necessary to include the properties you want to change- any properties left undefined on the configuration object (or its sub-objects) will take their default/parent value automatically.
 
 #### Default Configuration Values
+
 ```js
+
 const defaultConfig = {
   //globally enable/disable logging
   enabled: true,
 
-  //current level for this logger, as int or string name value- logs belonging to levels higher than this will be silenced (see LOG_LEVELS above)
+  //current level for this logger, as int or string name value- 
+  //logs belonging to levels higher than this will be silenced
+  //(see LOG_LEVELS above for possible values)
   level: DEFAULT_LOG_LEVEL,
 
-  //formatter for the comma separated arguments passed to the logging func (prefix is included as first param)
-  formatLogSegments: (elements) => elements.join(" "),
+  //formatter for the comma separated arguments, 
+  //should return the final message string
+  //(prefix is included as first param)
+  formatLogSegments: (elements) =>
+    elements.join(" "),
 
-  //settings related to the prefix for each message. pass 'prefix: false' to remove the prefix entirely
+  //settings related to the prefix for each message. 
+  //pass 'prefix: false' to remove the prefix entirely
   prefix: {
     includeLevelName: true,
     includeTime: true,
     getCurrentTimeString: () =>
-      new Date().toLocaleTimeString("en-US", { hour12: false }),
+      new Date().toLocaleTimeString("en-US", {
+        hour12: false,
+      }),
     getRestOfPrefix: () => [],
-    format: (segments) => `[${segments.join(" ")}]`,
+    format: (segments) =>
+      `[${segments.join(" ")}]`,
 
     //css color names or hex values per-level.
     //can just pass one or two and keep defaults for rest.
@@ -88,29 +100,39 @@ const defaultConfig = {
     endpoint: null,
 
     //function to pass each log to. 'sink: {func: false}' to disable.
-    func: ({ message: { asSegments, asString }, metadata }) => {
+    func: ({
+      message: { asSegments, asString },
+      metadata,
+    }) => {
       console.log(...asSegments);
     },
   },
 
   // The `metadata` property is either an object, or a function which returns an object,
   // containing arbitrary key/ value pairs that you want sent to your sink endpoint/function
-  // for each log message(by default , `clientTimestamp` is included.) It could be
-  // a reference to the environment/running application, debugging context, log categories/tags, etc. 
+  // for each log message(by default , `clientTimestamp` is included.)
+  // references to the environment/running application, debugging context, 
+  // log categories/ tags, etc.
   // Can be useful in conjunction with `filter`.
-  metadata: () => ({ clientTimestamp: Date.now() }),
+  metadata: () => ({
+    clientTimestamp: Date.now(),
+  }),
 
-  //disable the "merging" behavior for all config properties; replace the parent config completely
+  //disable the "merging" behavior for all config properties;
+  //replace the parent config completely
   replaceParentConfig: false,
 
   //disable the "merging" behavior for metadata properties
   replaceParentMetadata: false,
 
   //function for filtering logs based on message content/metadata
-  filter: ({ message: { asSegments, asString }, metadata }) => true,
+  filter: ({
+    message: { asSegments, asString },
+    metadata,
+  }) => true,
 };
-```
 
+```
 
 ### Extending Log Configuration
 
@@ -118,15 +140,18 @@ Each individual log call (e.g., `log.info()`) can also optionally be
 passed these config options. These will be merged with the values from the parent createLogger, with values from the individual log call taking precedence in the case of them both specifying a particular key.
 
 ```js
+
 log.debug(
   { prefix: { format: (parts: string[]) => `((${parts.join("__")})):` } },
   "a debug message"
 );
+
 ```
 
 Config/metadata can also be extended with the `createSubLogger` function, as shown in the example below.
 
 ````js
+
 import { createLogger } from "@leothorp/loggle";
 
 
@@ -155,3 +180,4 @@ To summarize, the overall precedence order for any specified config/metadata is:
 2. parent createSubLogger
 3. parent createLogger
 4. library defaults
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { post } from "./request.js";
 
 const mapObj = (obj, fn) => {
-  const mappedAsArr = Object.entries(obj).map(([k, v]) => fn(k, v));
+  const mappedAsArr = Object.entries(obj).map(
+    ([k, v]) => fn(k, v)
+  );
   return Object.fromEntries(mappedAsArr);
 };
-const invokeIfFunction = (val) => (typeof val === "function" ? val() : val);
+const invokeIfFunction = (val) =>
+  typeof val === "function" ? val() : val;
 
 const DEFAULT_LOG_LEVEL = "info";
 
@@ -20,20 +23,29 @@ const defaultConfig = {
   //globally enable/disable logging
   enabled: true,
 
-  //current level for this logger, as int or string name value- logs belonging to levels higher than this will be silenced (see LOG_LEVELS above)
+  //current level for this logger, as int or string name value- 
+  //logs belonging to levels higher than this will be silenced
+  //(see LOG_LEVELS above for possible values)
   level: DEFAULT_LOG_LEVEL,
 
-  //formatter for the comma separated arguments passed to the logging func (prefix is included as first param)
-  formatLogSegments: (elements) => elements.join(" "),
+  //formatter for the comma separated arguments, 
+  //should return the final message string
+  //(prefix is included as first param)
+  formatLogSegments: (elements) =>
+    elements.join(" "),
 
-  //settings related to the prefix for each message. pass 'prefix: false' to remove the prefix entirely
+  //settings related to the prefix for each message. 
+  //pass 'prefix: false' to remove the prefix entirely
   prefix: {
     includeLevelName: true,
     includeTime: true,
     getCurrentTimeString: () =>
-      new Date().toLocaleTimeString("en-US", { hour12: false }),
+      new Date().toLocaleTimeString("en-US", {
+        hour12: false,
+      }),
     getRestOfPrefix: () => [],
-    format: (segments) => `[${segments.join(" ")}]`,
+    format: (segments) =>
+      `[${segments.join(" ")}]`,
 
     //css color names or hex values per-level.
     //can just pass one or two and keep defaults for rest.
@@ -47,31 +59,42 @@ const defaultConfig = {
     },
   },
 
+  //settings for where the logger should send the log output
   sink: {
     //string URL of an endpoint the logger will POST logs to, if present.
     endpoint: null,
 
     //function to pass each log to. 'sink: {func: false}' to disable.
-    func: ({ message: { asSegments, asString }, metadata }) => {
+    func: ({
+      message: { asSegments, asString },
+      metadata,
+    }) => {
       console.log(...asSegments);
     },
   },
 
   // The `metadata` property is either an object, or a function which returns an object,
   // containing arbitrary key/ value pairs that you want sent to your sink endpoint/function
-  // for each log message(by default , `clientTimestamp` is included.) It could be
-  // a reference to the environment/running application, debugging context, log categories/tags, etc. 
+  // for each log message(by default , `clientTimestamp` is included.)
+  // references to the environment/running application, debugging context, 
+  // log categories/ tags, etc.
   // Can be useful in conjunction with `filter`.
-  metadata: () => ({ clientTimestamp: Date.now() }),
+  metadata: () => ({
+    clientTimestamp: Date.now(),
+  }),
 
-  //disable the "merging" behavior for all config properties; replace the parent config completely
+  //disable the "merging" behavior for all config properties;
+  //replace the parent config completely
   replaceParentConfig: false,
 
   //disable the "merging" behavior for metadata properties
   replaceParentMetadata: false,
 
   //function for filtering logs based on message content/metadata
-  filter: ({ message: { asSegments, asString }, metadata }) => true,
+  filter: ({
+    message: { asSegments, asString },
+    metadata,
+  }) => true,
 };
 
 const toObj = (arr, subObjSelector) =>
@@ -81,23 +104,35 @@ const toObj = (arr, subObjSelector) =>
   }, {});
 
 const isPlainObj = (val) =>
-  !Array.isArray(val) && typeof val === "object" && val !== null;
+  !Array.isArray(val) &&
+  typeof val === "object" &&
+  val !== null;
 const flattenObjHash = (obj) => {
   return toObj(Object.keys(obj), (k) => {
     if (isPlainObj(obj[k])) {
-      return { [k]: true, ...flattenObjHash(obj[k]) };
+      return {
+        [k]: true,
+        ...flattenObjHash(obj[k]),
+      };
     }
     return { [k]: true };
   });
 };
-const flattenedConfigKeysHash = flattenObjHash(defaultConfig);
+const flattenedConfigKeysHash = flattenObjHash(
+  defaultConfig
+);
 const isPlainConfigObj = (val) => {
   return (
     isPlainObj(val) &&
-    Object.keys(val).every((k) => flattenedConfigKeysHash.hasOwnProperty(k))
+    Object.keys(val).every((k) =>
+      flattenedConfigKeysHash.hasOwnProperty(k)
+    )
   );
 };
-const mergeConfigs = (defaultConfigVal, inputConfigVal) => {
+const mergeConfigs = (
+  defaultConfigVal,
+  inputConfigVal
+) => {
   if (inputConfigVal.replaceParentConfig) {
     return inputConfigVal;
   }
@@ -107,15 +142,29 @@ const mergeConfigs = (defaultConfigVal, inputConfigVal) => {
       inputConfigVal
     );
   }
-  const final = mapObj(defaultConfigVal, (k, v) => {
-    if (
-      isPlainConfigObj(defaultConfigVal[k]) &&
-      isPlainConfigObj(inputConfigVal[k])
-    ) {
-      return [k, mergeConfigs(defaultConfigVal[k], inputConfigVal[k])];
+  const final = mapObj(
+    defaultConfigVal,
+    (k, v) => {
+      if (
+        isPlainConfigObj(defaultConfigVal[k]) &&
+        isPlainConfigObj(inputConfigVal[k])
+      ) {
+        return [
+          k,
+          mergeConfigs(
+            defaultConfigVal[k],
+            inputConfigVal[k]
+          ),
+        ];
+      }
+      return [
+        k,
+        inputConfigVal.hasOwnProperty(k)
+          ? inputConfigVal[k]
+          : v,
+      ];
     }
-    return [k, inputConfigVal.hasOwnProperty(k) ? inputConfigVal[k] : v];
-  });
+  );
   return final;
 };
 
@@ -125,16 +174,23 @@ const getPrefixSegments = (levelName, config) => {
   }
   const prefixElements = [
     config.prefix.includeLevelName && levelName,
-    config.prefix.includeTime && config.prefix.getCurrentTimeString(),
+    config.prefix.includeTime &&
+      config.prefix.getCurrentTimeString(),
     ...config.prefix.getRestOfPrefix(),
   ];
 
-  const formatChar = config.prefix.colors ? "%c" : "";
-  const prefixStr = formatChar + config.prefix.format(prefixElements);
+  const formatChar = config.prefix.colors
+    ? "%c"
+    : "";
+  const prefixStr =
+    formatChar +
+    config.prefix.format(prefixElements);
   const css = config.prefix.colors
     ? `color: ${config.prefix.colors[levelName]};`
     : "";
-  const prefixSegments = css ? [prefixStr, css] : [prefixStr];
+  const prefixSegments = css
+    ? [prefixStr, css]
+    : [prefixStr];
   return prefixSegments;
 };
 
@@ -154,35 +210,56 @@ const normalizeLevel = (levelConfigVal) => {
   return LOG_LEVELS[levelConfigVal];
 };
 
-const createLogger = (rawInputConfig = defaultConfig) => {
-  const globalConfig = mergeConfigs(defaultConfig, rawInputConfig);
+const createLogger = (
+  rawInputConfig = defaultConfig
+) => {
+  const globalConfig = mergeConfigs(
+    defaultConfig,
+    rawInputConfig
+  );
   const makeLevelLogger = (levelName, intVal) => {
     return (...baseLogArgs) => {
       //TODO(lt): vvv how differentiate another obj they want to send with same keys?
       //localOverrideKeys is for that.
 
-      const hasAdditionalConfig = isPlainConfigObj(baseLogArgs[0]);
+      const hasAdditionalConfig =
+        isPlainConfigObj(baseLogArgs[0]);
 
       const config = hasAdditionalConfig
-        ? mergeConfigs(globalConfig, baseLogArgs[0])
+        ? mergeConfigs(
+            globalConfig,
+            baseLogArgs[0]
+          )
         : globalConfig;
 
-      const logArgs = hasAdditionalConfig ? baseLogArgs.slice(1) : baseLogArgs;
+      const logArgs = hasAdditionalConfig
+        ? baseLogArgs.slice(1)
+        : baseLogArgs;
 
-      if (!config.enabled || intVal > normalizeLevel(config.level)) {
+      if (
+        !config.enabled ||
+        intVal > normalizeLevel(config.level)
+      ) {
         return;
       }
-      const metadata = invokeIfFunction(config.metadata);
+      const metadata = invokeIfFunction(
+        config.metadata
+      );
 
       const allLogSegments = [
         ...getPrefixSegments(levelName, config),
         ...logArgs,
       ].filter((x) => x);
 
-      const asString = config.formatLogSegments(allLogSegments);
+      const asString = config.formatLogSegments(
+        allLogSegments
+      );
       if (
         !config.filter({
-          message: { asString, asSegments: allLogSegments },
+          message: {
+            asString,
+            asSegments: allLogSegments,
+          },
           metadata,
         })
       ) {
@@ -190,7 +267,10 @@ const createLogger = (rawInputConfig = defaultConfig) => {
       }
       if (config.sink.func) {
         config.sink.func({
-          message: { asString, asSegments: allLogSegments },
+          message: {
+            asString,
+            asSegments: allLogSegments,
+          },
           metadata,
         });
       }
@@ -198,21 +278,32 @@ const createLogger = (rawInputConfig = defaultConfig) => {
         //TODO: batch option for sending sink endpoint requests
 
         post(config.sink.endpoint, {
-          message: { asString, asSegments: allLogSegments },
+          message: {
+            asString,
+            asSegments: allLogSegments,
+          },
           metadata,
         });
       }
     };
   };
 
-  const levelLoggers = mapObj(LOG_LEVELS, (name, intValue) => {
-    return [name, makeLevelLogger(name, intValue)];
-  });
+  const levelLoggers = mapObj(
+    LOG_LEVELS,
+    (name, intValue) => {
+      return [
+        name,
+        makeLevelLogger(name, intValue),
+      ];
+    }
+  );
 
   return {
     ...levelLoggers,
     createSubLogger: (subConfig) => {
-      return createLogger(...mergeConfigs(globalConfig, subConfig));
+      return createLogger(
+        ...mergeConfigs(globalConfig, subConfig)
+      );
     },
   };
 };
